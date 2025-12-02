758f5f8fed1c485266304f98bfd23055b83255d7
chiddarwargauri <chiddarwargauri@gmail.com>
2025-12-02 22:35:56 +0530

    Day 2- reverse string

--- src/string/com/BasicReverseString.java
+++ src/string/com/BasicReverseString.java
@@ -5,7 +5,22 @@ public class BasicReverseString {
 	public static void main(String args[]) {
 		
 		
-		
+		 Scanner sc= new Scanner(System.in) ;
+		 
+		 System.out.println("Enter string :-");
+		 
+		 String s1=sc.nextLine();
+		 
+		 String reverse=" ";
+		 
+		 for(int i=s1.length()-1;i>=0;i--) {
+			 
+			  reverse+=s1.charAt(i);
+		 }
+		 
+		 System.out.print("Reverse String :-"+reverse);
+			 
+			 sc.close();
 		
 	}
 
